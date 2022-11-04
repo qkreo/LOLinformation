f0a6ecb5d6c47dd55812998ced27b4d4dee0605b
DESKTOP-TGHAAU4\박ㄷ현 <bdh407@naver.com>
2022-11-04 12:49:35 +0900

    박대현: 초기셋팅

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const http = require('http').createServer(app); 
+
+const Router = require('./routes/index');
+const errorHandlerMiddleware = require('./middlewares/error_handler_middleware');
+
+app.use(express.json());
+
+app.use('/', Router);
+app.use(errorHandlerMiddleware); // 에러핸들러
+
+http.listen(port, () => {
+    console.log(`${port}번 포트로 서버 실행`);
+});
