eb4187cef6ea56cb58dd3c4c5ff651058f9532c8
Sky-Park <alsrb7018@gmail.com>
2022-12-10 14:24:22 +0900

    경로수정 박민규

--- front-end/src/Constants.js
+++ front-end/src/Constants.js
@@ -1,3 +1,3 @@
 export const RIOTGAMES_API = 'https://kr.api.riotgames.com/lol'
 export const API_KEY = "RGAPI-a45b08dd-4e70-462b-a04c-ae1039a4bfe5";
-export const SERVER_URL = "http://54.180.83.225:5000"
+export const SERVER_URL = "https://sparta-pmg.shop"
